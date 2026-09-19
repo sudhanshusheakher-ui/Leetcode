10853e91db233490ad56357021765e9563f65081
sudhanshusheakher-ui <235108244+sudhanshusheakher-ui@users.noreply.github.com>
2026-09-19 20:49:32 +0530

    Time: 0 ms (100.00%) | Memory: 27.3 MB (51.99%) - LeetSync

--- 344-reverse-string/reverse-string.cpp
+++ 344-reverse-string/reverse-string.cpp
@@ -3,7 +3,7 @@ public:
     void reverseString(vector<char>& s) {
         int ptr1=0;
         int ptr2=s.size()-1;
-        vector<char>rev;
+    
             while(ptr1<ptr2){
                 swap(s[ptr1],s[ptr2]);
                 ptr1++;
