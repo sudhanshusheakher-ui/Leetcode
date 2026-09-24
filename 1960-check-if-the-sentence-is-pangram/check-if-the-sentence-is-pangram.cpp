f9e16e53a2e7547ea7c0cd378b98abd39a26d335
sudhanshusheakher-ui <235108244+sudhanshusheakher-ui@users.noreply.github.com>
2026-09-24 22:17:04 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (60.11%) - LeetSync

---
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool checkIfPangram(string sentence) {
+    int hash[26]={0};
+    for(int i=0;i<sentence.length();i++){
+        hash[sentence[i]-'a']++;
+    
+    }
+    for(int i=0;i<26;i++){
+       if(hash[i]==0) {
+        return false;
+        }
+
+
+
+    }
+            return true;
+      
+    }
+};
