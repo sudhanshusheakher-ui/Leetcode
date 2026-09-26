819fef3e61b0a50c4a6e73bdcce4cd65062ee1fd
sudhanshusheakher-ui <235108244+sudhanshusheakher-ui@users.noreply.github.com>
2026-09-26 23:12:51 +0530

    Time: 6 ms (21.69%) | Memory: 9.9 MB (11.11%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -3,8 +3,8 @@ public:
     bool isAnagram(string s, string t) {
         if(s.length() != t.length()) return false;
         unordered_map<char,int> mpp;
-        for(int i=0;i<s.size();i++){
-          mpp[s[i]]++; 
+        for(char c :s){
+          mpp[c]++; 
         }
         for(char c:t){
             mpp[c]--;
